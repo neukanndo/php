6167948b878653b61a4682a6574436ac0bb546ee
Neukanndo <neukanndo@gmail.com>
2017-11-11 14:45:42 +0530

    Barcode generate using php

---
+++ index.php
@@ -0,0 +1,21 @@
+<div>1 . Display barcode with random product number <br>
+<img alt="Coding Sips" src="barcode.php?text=ProductNo:-9034092348023&print=true&size=40" />
+</div>
+
+<br>
+<br>
+<div>
+2. Display barcode with code type :- Code39 <br>
+<img alt="Coding Sips" src="barcode.php?codetype=Code39&size=50&text=Coding-sips-item-no-324324&print=true" />
+</div>
+
+<br>
+<br>
+<div>
+3. Display barcode with code type :- Code128 <br>
+<img alt="Coding Sips" src="barcode.php?codetype=Code128&size=50&text=Coding-sips-item-no-234234234&print=true" />
+</div>
+
+4. Display barcode in verticle order <br>
+<img alt="Coding Sips" src="barcode.php?codetype=Code128&size=30&text=Coding-sips&orientation=vertical" />
+</div>
